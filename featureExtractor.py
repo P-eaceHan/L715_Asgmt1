cc0009f9f0ada33f225b85c66522430eb81a82fa
Peace Han <peacehan@gmail.com>
2019-02-20 21:29:26 -0500

    full vector generated

--- featureExtractor.py
+++ featureExtractor.py
@@ -2,40 +2,44 @@
 extracting features from training data:
 [w-2   w-1   w+1   w+2   w-2%w-1   w-1%w+1   w+1%w+2   class]
 '''
+import regex as re
 import xml.etree.ElementTree as ET
-import re
+
 inputFile = 'train_data/armN.train'
 
-# with open(inputFile) as file:
-#     for line in file:
-#         print(line)
-
-# e = ET.parse(inputFile).getroot()
-# # print(e)
-#
-# # for atype in e:
-# #     print(atype.tag, atype.attrib)
-# out = []
-#
-# for instance in e.findall('instance'):
-#     print(instance.attrib)
-#     # out.append(instance.get('answer'))
-#     # print(instance.find('answer'))
-#     ans = instance.find('answer').attrib.get('senseid')
-#     print(instance.find('context').text)
-#     # print(ans)
-
-# regex approach
-# pattern = "(.+?)<head>(.+?)</head>(.+?)"
+# 2 words before and after word
+# punctuation not count as word
 pattern = "(?:\w+\W+){,2}<head>arms?</head>(?:\W+\w+){,2}"
-# pattern2 = "<head>(?:\W+\w+){,2}"
-# pattern = "(?P<w-2>"
+
+# 2 words before and after word
+# punctuation = word
+pattern2 = "(?:\S+\s+){,2}<head>arms?<\/head>(?:\s+\S+){,2}"
+
+out = open("armNVector.train", 'w')
+justWords = []
+
+# what to do with instance 00078497? it's two instances of arms in one context
 with open(inputFile) as file:
     for line in file:
         res = re.findall(pattern, line)
-        # res2 = re.findall(pattern2, line)
-        if res:
-            print(res)
-        # if res2:
-        #     print(res2)
+        res2 = re.findall(pattern2, line)
+        # if res:
+        #     print(res)
+        if res2:
+            # print(res2)
+            [justWords.append(x) for x in res2]
+            # [out.write(x + '\n') for x in res2]
+
+    for tempVec in justWords:
+        tempVecList = tempVec.split()
+        if len(tempVecList) < 5:  # to detect potential errors
+            out.write(' '.join(tempVecList))
+        tempVecList.pop(2)
+        tempVec = ' '.join(tempVecList)
+        for i in range(len(tempVecList)-1):
+            tempVec += ' ' + tempVecList[i] + '%' + tempVecList[i+1]
+        print(tempVec)
+
+
+print(justWords)
 
