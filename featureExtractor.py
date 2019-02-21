0258a8a6215f43f7b73ff18512ec5302e60bb4a5
Peace Han <peacehan@gmail.com>
2019-02-21 16:59:08 -0500

    combining regex and xml parser

--- featureExtractor.py
+++ featureExtractor.py
@@ -7,6 +7,26 @@ import xml.etree.ElementTree as ET
 
 inputFile = 'train_data/armN.train'
 
+root = ET.parse(inputFile).getroot()
+for instance in root.findall('instance'):
+    answer = instance.find('answer').attrib
+    inst = answer.get('instance')
+    sense = answer.get('senseid')
+    context = ET.tostring(instance.find('context'))
+    context = context.decode('UTF-8')
+    print(context)
+    # 2 words before and after word
+    # punctuation = word
+    pattern2 = "(?:\S+\s+){,2}<head>arms?<\/head>(?:\s+\S+){,2}"
+    res2 = re.findall(pattern2, context)
+    justWords = []
+    if res2:
+        print(res2)
+        [justWords.append(x) for x in res2]
+        print(justWords)
+        # [out.write(x + '\n') for x in res2]
+
+'''
 # 2 words before and after word
 # punctuation not count as word
 pattern = "(?:\w+\W+){,2}<head>arms?</head>(?:\W+\w+){,2}"
@@ -16,6 +36,7 @@ pattern = "(?:\w+\W+){,2}<head>arms?</head>(?:\W+\w+){,2}"
 pattern2 = "(?:\S+\s+){,2}<head>arms?<\/head>(?:\s+\S+){,2}"
 
 out = open("armNVector.train", 'w')
+out2 = open('arnmN.tsv', 'w')
 justWords = []
 
 # what to do with instance 00078497? it's two instances of arms in one context
@@ -35,11 +56,16 @@ with open(inputFile) as file:
         if len(tempVecList) < 5:  # to detect potential errors
             out.write(' '.join(tempVecList))
         tempVecList.pop(2)
-        tempVec = ' '.join(tempVecList)
+        tempVec = '\t'.join(tempVecList)
         for i in range(len(tempVecList)-1):
-            tempVec += ' ' + tempVecList[i] + '%' + tempVecList[i+1]
+            tempVec += '\t' + tempVecList[i] + '%' + tempVecList[i+1]
         print(tempVec)
+        tempVec += '\n'
+        out2.write(tempVec)
 
 
-print(justWords)
+
+# print(justWords)
+
+'''
 
