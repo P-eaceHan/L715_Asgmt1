9a9f593536e4eee34f8985534d72ca7b6abddee8
Peace Han <peacehan@gmail.com>
2019-03-01 11:55:17 -0500

    various kernels implemented

--- armClassifier.py
+++ armClassifier.py
@@ -2,6 +2,7 @@
 from sklearn import preprocessing
 from sklearn import svm
 from sklearn import metrics
+from sklearn.metrics import classification_report
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -28,7 +29,6 @@ def encode(filepath, isY):
     if isY:
         enc = preprocessing.LabelEncoder()
     else:
-        # enc = preprocessing.OneHotEncoder(n_values=shape)
         enc = preprocessing.OneHotEncoder(handle_unknown='ignore')
     out = enc.fit_transform(out)
     return out, enc
@@ -49,11 +49,9 @@ def encode_test(filepath, encoder, isY):
         if isY:
             line = line.split()
             label = [line[-1]]
-            # label = encoder.transform(label)
             out.extend(label)
         else:
             line = line.split('\t')
-            # vectored_line = encoder.transform(line)
             out.append(line)
     out = encoder.transform(out)
     return out
@@ -66,29 +64,21 @@ def arm_classifier():
     isY = False
     trainingFile = 'train_data/{0}/{0}.tsv'.format(dat)
     x_train, enc_data = encode(trainingFile, isY)
-    # x_train = get_data(trainingFile, enc, isY)
-    # x_train = list(preprocessing.scale(x_train))
 
     # getting y_train
     isY = True
     trainingLabels = 'train_data/{0}/{0}Answers.train'.format(dat)
     y_train, enc_label = encode(trainingLabels, isY)
-    # y_train = get_data(trainingLabels, enc, isY)
-    # y_train = list(preprocessing.scale(y_train))
 
     # getting x_test
     isY = False
     testingFile = 'test_data/{0}/{0}_test.tsv'.format(dat)
     x_test = encode_test(testingFile, enc_data, isY)
-    # x_test = get_data(testingFile, enc, isY)
-    # x_test = list(preprocessing.scale(x_test))
 
     # getting y_test
     isY = True
     testingLabels = 'test_data/{0}/{0}Answers.test'.format(dat)
     y_test = encode_test(testingLabels, enc_label, isY)
-    # y_test = get_data(testingLabels, enc, isY)
-    # y_test = list(preprocessing.scale(y_test))
 
     # print('x_train: \n', x_train)
     # print('y_train: \n', y_train)
@@ -96,33 +86,58 @@ def arm_classifier():
     # print(y_test)
 
     # interpreting encoded labels
-    # print('og  labels: ', enc.categories_)
-    # print('num labels: ', enc.transform(enc.categories_))
+    print('labels: ')
+    [print(x) for x in enc_label.classes_]
+    # print('num labels: ', enc_label.transform(enc_label.classes_))
+    print('===============================================')
 
-    # svm SVC classifier
-    # clf= svm.SVC()
-    # clf = svm.SVC(kernel='sigmoid', gamma='scale', C=3, class_weight='balanced', decision_function_shape='ovo')
-    clf = svm.SVC(kernel='rbf', gamma='scale', C=4)
+    # rbf kernel
+    print('rbf kernel results')
+    # clf= svm.SVC(kernel='rbf')
+    clf = svm.SVC(kernel='rbf', gamma='scale', C=4, class_weight='balanced', decision_function_shape='ovo')
     clf.fit(x_train, y_train)
     y_pred = clf.predict(x_test)
-
-    # svm Linear SVC classifier
-    # clf = svm.LinearSVC()
-    # clf = svm.LinearSVC(loss='hinge', max_iter=5000000)
-    # clf.fit(x_train, y_train)
-    # y_pred = clf.predict(x_test)
-
-    # svm Nu SVC classifier
-    # clf = svm.NuSVC(nu=0.9999)
-    # clf.fit(x_train, y_train)
-    # y_pred = clf.predict(x_test)
-
-    print(set(y_pred))
     print('Did not predict: ', set(y_test) - set(y_pred))
 
+    print(classification_report(y_test, y_pred))
     print('Accuracy: ', metrics.accuracy_score(y_test, y_pred))
-    print('Precision: ', metrics.precision_score(y_test, y_pred, average='weighted'))
-    print('Recall: ', metrics.recall_score(y_test, y_pred, average='weighted'))
+    print('===============================================')
+
+    # linear kernel
+    print('linear kernel results')
+    # clf= svm.SVC(kernel='linear)
+    clf = svm.SVC(kernel='linear', gamma='scale', C=4, class_weight='balanced', decision_function_shape='ovo')
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    print('Did not predict: ', set(y_test) - set(y_pred))
+
+    print(classification_report(y_test, y_pred))
+    print('Accuracy: ', metrics.accuracy_score(y_test, y_pred))
+    print('===============================================')
+
+    # sigmoid? poly? kernel
+    print('sigmoid kernel results')
+    # clf= svm.SVC(kernel='poly)
+    clf = svm.SVC(kernel='sigmoid', gamma='scale', C=4, class_weight='balanced', decision_function_shape='ovo')
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    print('Did not predict: ', set(y_test) - set(y_pred))
+
+    print(classification_report(y_test, y_pred))
+    print('Accuracy: ', metrics.accuracy_score(y_test, y_pred))
+    print('===============================================')
+
+    # poly kernel
+    print('poly kernel results')
+    # clf= svm.SVC(kernel='poly')
+    clf = svm.SVC(kernel='poly', gamma='scale', C=4, class_weight='balanced', decision_function_shape='ovo')
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    print('Did not predict: ', set(y_test) - set(y_pred))
+
+    print(classification_report(y_test, y_pred))
+    print('Accuracy: ', metrics.accuracy_score(y_test, y_pred))
+    print('===============================================')
 
 
 def main():
