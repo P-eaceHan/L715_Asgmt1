9a451c7a438039d268d38d9df0810aece917f02f
Peace Han <peacehan@gmail.com>
2019-03-01 11:32:08 -0500

    classifier with OneHotEncoder

--- armClassifier.py
+++ armClassifier.py
@@ -7,7 +7,12 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def get_encoder(filepath, isY):
+'''
+:param filepath: file to the tsv file of features
+:param isY: flag telling if this is a label file
+:return: numericized features and encoder
+'''
+def encode(filepath, isY):
     with open(filepath) as tsv:
         data = tsv.readlines()
     out = []
@@ -19,17 +24,23 @@ def get_encoder(filepath, isY):
             out.append([label])
         else:
             line = line.split('\t')
-            out.extend(line)
-    # print(out)
-    encoder = preprocessing.LabelEncoder()
-    # encoder = preprocessing.OneHotEncoder()
-    # encoder = CountVectorizer()
-    encoder.fit(out)
-    # transformed_train_vocab = encoder.transform(encoder.classes_)  # do we need this?
-    return encoder
+            out.append(line)
+    if isY:
+        enc = preprocessing.LabelEncoder()
+    else:
+        # enc = preprocessing.OneHotEncoder(n_values=shape)
+        enc = preprocessing.OneHotEncoder(handle_unknown='ignore')
+    out = enc.fit_transform(out)
+    return out, enc
 
 
-def get_data(filepath, encoder, isY):
+'''
+:param filepath: file to the tsv file of features
+:param encoder: the encoder to encode data in filepath
+:param isY: flag telling if this is a label file
+:return: numericized test features
+'''
+def encode_test(filepath, encoder, isY):
     with open(filepath) as tsv:
         data = tsv.readlines()
     out = []
@@ -38,12 +49,13 @@ def get_data(filepath, encoder, isY):
         if isY:
             line = line.split()
             label = [line[-1]]
-            label = encoder.transform(label)
+            # label = encoder.transform(label)
             out.extend(label)
         else:
             line = line.split('\t')
-            vectored_line = encoder.transform(line)
-            out.append(list(vectored_line))
+            # vectored_line = encoder.transform(line)
+            out.append(line)
+    out = encoder.transform(out)
     return out
 
 
@@ -53,43 +65,44 @@ def arm_classifier():
     # getting x_train
     isY = False
     trainingFile = 'train_data/{0}/{0}.tsv'.format(dat)
-    enc = get_encoder(trainingFile, isY)
-    x_train = get_data(trainingFile, enc, isY)
+    x_train, enc_data = encode(trainingFile, isY)
+    # x_train = get_data(trainingFile, enc, isY)
     # x_train = list(preprocessing.scale(x_train))
 
     # getting y_train
     isY = True
     trainingLabels = 'train_data/{0}/{0}Answers.train'.format(dat)
-    enc = get_encoder(trainingLabels, isY)
-    y_train = get_data(trainingLabels, enc, isY)
+    y_train, enc_label = encode(trainingLabels, isY)
+    # y_train = get_data(trainingLabels, enc, isY)
     # y_train = list(preprocessing.scale(y_train))
 
     # getting x_test
     isY = False
     testingFile = 'test_data/{0}/{0}_test.tsv'.format(dat)
-    enc = get_encoder(testingFile, isY)
-    x_test = get_data(testingFile, enc, isY)
+    x_test = encode_test(testingFile, enc_data, isY)
+    # x_test = get_data(testingFile, enc, isY)
     # x_test = list(preprocessing.scale(x_test))
 
     # getting y_test
     isY = True
     testingLabels = 'test_data/{0}/{0}Answers.test'.format(dat)
-    enc = get_encoder(testingLabels, isY)
-    y_test = get_data(testingLabels, enc, isY)
+    y_test = encode_test(testingLabels, enc_label, isY)
+    # y_test = get_data(testingLabels, enc, isY)
     # y_test = list(preprocessing.scale(y_test))
 
-    # print(x_train)
-    # print(y_train)
+    # print('x_train: \n', x_train)
+    # print('y_train: \n', y_train)
     # print(x_test)
     # print(y_test)
 
     # interpreting encoded labels
-    # print('og  labels: ', enc.classes_)
-    # print('num labels: ', enc.transform(enc.classes_))
+    # print('og  labels: ', enc.categories_)
+    # print('num labels: ', enc.transform(enc.categories_))
 
     # svm SVC classifier
     # clf= svm.SVC()
-    clf = svm.SVC(kernel='sigmoid', gamma='scale', C=3, class_weight='balanced', decision_function_shape='ovo')
+    # clf = svm.SVC(kernel='sigmoid', gamma='scale', C=3, class_weight='balanced', decision_function_shape='ovo')
+    clf = svm.SVC(kernel='rbf', gamma='scale', C=4)
     clf.fit(x_train, y_train)
     y_pred = clf.predict(x_test)
 
